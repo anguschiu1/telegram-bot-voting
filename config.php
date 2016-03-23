190bdf7f7928e134358728f8840fcbed797466ef
Goofyz <rwcleung@gmail.com>
2016-03-23 23:32:11 +0800

    Add config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BOT_TOKEN', 'MY_AWESOME_BOT_TOKEN');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+define('WEBHOOK_URL', 'https://my-site.example.com/secret-path-for-webhooks/');
+
+define('DB_CONNECT_STR', 'PDO_DB_CONNECT_STR');
+
+?>
