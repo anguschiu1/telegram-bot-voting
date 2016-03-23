6e6980acc07884e1538b3e13eff49fa8f7bdde99
Goofyz <rwcleung@gmail.com>
2016-03-23 23:37:13 +0800

    Add database related funcion. * `addRecords()` to insert a records. * 'getResult()` to retrieve the voting result.

---
+++ db.php
@@ -0,0 +1,40 @@
+<?php
+
+function getDb(){
+    //$db = new PDO('mysql:host=localhost;dbname=bot;charset=utf8mb4', 'root', '');
+    $db = new PDO(DB_CONNECT_STR);
+    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    
+    return $db;
+}
+function addRecords($userId, $userName, $poll){
+    $db = getDb();
+	
+    $stmt = $db->prepare("INSERT INTO polling(User_ID, User_Name, Poll, Last_Modified_Date) VALUES (?, ?, ?, NOW())");
+    $stmt->execute(array($userId, $userName, $poll));
+    $insertId = $db->lastInsertId();
+    
+    $db = null;
+}
+
+function getResult(){
+    $db = getDb();
+    $stmt = $db->query('SELECT poll, count(1) AS total FROM polling GROUP BY poll ORDER BY poll');
+ 
+    $ret['1'] = 0;
+    $ret['2'] = 0;
+    $ret['3'] = 0;
+    $ret['4'] = 0;
+    $ret['5'] = 0;
+    $ret['6'] = 0;
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $ret[$row['poll']] = $row['total'];
+    }
+    
+    $db = null;
+    
+    return $ret;
+}
+?>
