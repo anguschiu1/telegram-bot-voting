6b413e454334d776da59c43bb80165812c4d71a7
Goofyz <rwcleung@gmail.com>
2016-04-02 20:19:37 +0800

    Require files from the `pollbot.php` instead of `config.php`

--- config.php
+++ config.php
@@ -1,16 +1,15 @@
 <?php
 
-define('BOT_TOKEN', 'MY_AWESOME_BOT_TOKEN');
+define('BOT_TOKEN', '217706820:AAHRbU7SJnPVjtMmKcTVYhlFeJycysVjJrI');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 define('WEBHOOK_URL', 'https://my-site.example.com/secret-path-for-webhooks/');
 
-define('DB_CONNECT_STR', 'PDO_DB_CONNECT_STR');
-define('DB_USER_NAME', 'Username');
-define('DB_PASSWORD', 'Password');
+define('DB_CONNECT_STR', 'mysql:host=localhost;dbname=bot;charset=utf8mb4');
+//define('DB_CONNECT_STR', 'pgsql:host=localhost;dbname=bot;user=poll;password=abcd1234;');
+define('DB_USER_NAME', 'root');
+define('DB_PASSWORD', '');
 
 define('DEBUG', true);
 define('DEBUG_FILE_NAME', 'debug.txt');
 
-require('db.php');
-require('process.php');
 ?>
