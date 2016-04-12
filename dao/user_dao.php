f5c0bb8f13827b970e97612d11b0fdb570a18949
Goofyz <rwcleung@gmail.com>
2016-04-13 01:22:24 +0800

    Move the `User` related DB method to `dao/user_dao.php`

---
+++ dao/user_dao.php
@@ -0,0 +1,53 @@
+<?php
+class UserDao{
+    
+    public static function get($id){
+        $db = getDb();
+        $stmt = $db->prepare('SELECT user_id, user_name, first_name, last_name, chat_id, ip, authorized, create_date, last_modified_date FROM voter WHERE user_id = ?');
+        $stmt->execute(array($id));
+        
+        $ret = null;
+        if($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $user = new User($row);
+            return $user;
+        }
+        
+        return null;
+    }
+
+    public static function create($obj){
+        $db = getDb();
+        
+        $stmt = $db->prepare("INSERT INTO voter(user_id, user_name, first_name, last_name, chat_id, authorized, ip, create_date, last_modified_date) VALUES (?, ?, ?,?, ?, ?, ?, NOW(), NOW())");
+        $stmt->execute(array($obj->user_id, $obj->user_name, $obj->first_name, $obj->last_name, $obj->chat_id, $obj->authorized, $_SERVER['REMOTE_ADDR']));
+        $insertId = $db->lastInsertId();
+        
+        $db = null;
+        
+        $obj->user_id = $insertId;
+        
+        return $obj;
+    }
+
+    public static function update($obj){
+        $db = getDb();
+
+
+        $stmt = $db->prepare("update voter set user_name = ? , first_name = ?, last_name = ?, chat_id = ?, authorized = ?, ip = ?, last_modified_date = NOW() where user_id = ?");
+        $stmt->execute(array($obj->user_name, $obj->first_name, $obj->last_name, $obj->chat_id, $obj->authorized, $_SERVER['REMOTE_ADDR'], $obj->user_id));
+        
+        $db = null;
+        
+        return $obj;
+    }
+    
+    public static function save($obj){
+        if($obj->create_date){
+            return self::update($obj);
+        }
+        else{
+            return self::create($obj);
+        }
+    }
+}
+?>
---
+++ model/user_model.php
@@ -0,0 +1,33 @@
+<?php
+class User{
+    public $user_id;
+    public $user_name;
+    public $first_name;
+    public $last_name;
+    public $chat_id;
+    public $authorized;
+    public $ip;
+    public $create_date;
+    public $last_modified_date;
+
+    function __construct(){
+        $a = func_get_args();
+        $i = func_num_args();
+        if (method_exists($this,$f='__construct'.$i)) {
+            call_user_func_array(array($this,$f),$a);
+        }
+    }
+
+    function __construct1($array) {
+       $this->user_id = $array['user_id'];
+       $this->user_name = $array['user_name'];
+       $this->first_name = $array['first_name'];
+       $this->last_name = $array['last_name'];
+       $this->chat_id = $array['chat_id'];
+       $this->authorized = $array['authorized'];
+       $this->ip = $array['ip'];
+       $this->create_date = $array['create_date'];
+       $this->last_modified_date = $array['last_modified_date'];
+    }
+}
+?>
