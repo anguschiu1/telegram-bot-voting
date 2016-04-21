afebe034992f63884394788e51b0a83a4a574fa5
Ryan Leung <rwcleung@gmail.com>
2016-04-21 10:22:39 +0800

    Fix the invitation link problem

--- lang_zh.php
+++ lang_zh.php
@@ -5,7 +5,7 @@
     $GLOBALS['WORD_TC']['INVITATION_MSG'] = '由於你是第一次投票，你可以邀請 %d 個支持民主的朋友來投票，請複製以下訊息給你 Telegram、WhatsApp 或 Facebook 上的朋友。';
     $GLOBALS['WORD_TC']['INVITATION_LINK'] = "我誠意邀請你去「公民數據」查看立法會最新投票調查結果。\n\n".
             "Civic Data HK 公民數據以滾動投票方式讓支持民主的選民即時投票和睇結果。你可查看各政團的最新支持度，以求在九月四號的立法會選舉，做更明智的投票決定。\n\n".
-            "%s";
+            '[%1$s](%1$s)';
     $GLOBALS['WORD_TC']['SURVEY_NOT_START'] = '你還未開始投票，請使用 /start 開始。';
     $GLOBALS['WORD_TC']['SURVEY_Q1'] = '你的選民登記屬於那個選區？';
     $GLOBALS['WORD_TC']['SURVEY_Q1_NOT_AGREE'] = '我們需要你的同意，才能繼續。';
@@ -15,7 +15,7 @@
     $GLOBALS['WORD_TC']['SURVEY_THANKS_REMIND'] = '請於下個月一號回來再投票，同時看最新投票結果。到時我們會再提醒你，謝謝。';
     $GLOBALS['WORD_TC']['SURVEY_RESULT'] = "「`%s`」 選區的結果 (共 %d 票)：\n\n";
     $GLOBALS['WORD_TC']['SURVEY_RESULT_MORE'] = '還有其他 ';
-    $GLOBALS['WORD_TC']['SURVEY_RESULT_LINK'] = '([詳細的結果](http://civic-data.hk/result-graph/))';
+    $GLOBALS['WORD_TC']['SURVEY_RESULT_LINK'] = '([詳細的結果](http://votsonar.civicdata.hk/result/))';
     $GLOBALS['WORD_TC']['SURVEY_RESULT_RESTART_INSTRUCTION'] = "\n如要更改你的投票，請使用 /vote 重新選擇";
     $GLOBALS['WORD_TC'][''] = '';
     
