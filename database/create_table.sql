ab4ef178157c537da1f989eb21fa9c97c3735784
dickanf <dickanf@gmail.com>
2016-04-25 21:11:26 +0800

    Add new table for bulk message sending
    Add new table for bulk message sending

--- database/create_table.sql
+++ database/create_table.sql
@@ -48,3 +48,27 @@ create table invitation_user(
     last_modified_date timestamp not null,
     primary key (invitation_id, user_id)
 );
+
+create table bulk (
+    bulk_id int not null,
+    chat_id int not null,
+    lang varchar(2) not null default 'tc',
+    status int(1) not null default 1, -- 0=Done, 1=Create, 2=Processing, 3=Error
+    last_modified_date timestamp not null default CURRENT_TIMESTAMP
+);
+
+create table media_content (
+    bulk_id int not null,
+    media_type int(1) not null default 1,
+        -- 1=Text Message, 2=Photo, 3=Audio, 4=Document, 5=Sticker, 6=Video, 7=Voice, 8=Venue, 9=Contact, 10=ChatAction
+        -- Please don't declare multiple type in same bulk_id
+    lang varchar(2) not null default '*', -- * means all languages
+    media_content varchar(5000),
+    media_status int not null default 1
+        -- 0=DISABLED, 1=INIT, 2=SAMPLE, 3=APPROVED
+        --      INIT - Ready for sending sample for review
+        --      SAMPLE - After sample sendout
+        --      APPROVED - Available for bulk sending
+        --      DISABLED - Bulk sending job will ignore the record, any status can change to DISABLED
+        -- Media Lifecycle: INIT --> SAMPLE --> APPROVED
+);
