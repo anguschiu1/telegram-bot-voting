e1b8211abf8ccbf374203e975d52963782e2810b
dickanf <dickanf@gmail.com>
2016-04-25 22:36:10 +0800

    temp version

---
+++ script/bulk_content.php
@@ -0,0 +1,47 @@
+<?php
+
+$cycle_volume=1500;
+$batch_volume=29;
+$batch_wait=200000;
+
+$batch_count=$batch_volumn;
+
+require('../php/inc.php');
+require('module.php');
+
+$db = getDb();
+$stmt1 = $db->prepare("select * from bulk where status != 0 order by bulk_id, lang limit :cycle_volume");
+$stmt1->execute(array($cycle_volume));
+
+$i_bulk_id=null;
+$i_lang=null;
+while($row = $stmt1->fetch(PDO::FETCH_ASSOC)) {
+        echo "bulk_id=". $row["bulk_id"]. " chat_id=". $row["chat_id"]. " lang=". $row["lang"]. " status=". $row["status"]. " last_modified_date=". $row["last_modified_date"]. "\n";
+        if ( $row["bulk_id"] != $i_bulk_id || $row["lang"] != i_lang ) {
+                $stmt2 = $db->prepare("select * from media_content where bulk_id=:bulk_id and lang in ('*', :lang") order by lang desc limit 1");
+
+                $stmt2.bindValue(1, $row["bulkd_id"]);
+                $stmt2.bindParam(2, $row["lang"], PDO::PARAM_STR);
+                $stmt2->execute();
+                $media = $stmt2->fetch(PDO::FETCH_ASSOC);
+                echo "bulk_id=". $media["bulk_id"]. " media_type=". $media["media_type"]. " lang=". $media["lang"]. " media_status=". $media["media_status"]. "\n";
+                $i_bulk_id = $row["bulk_id"];
+                $i_lang = $row["lang"];
+        }
+
+        if ( $media["media_status"] == 3 ) {
+                sendMessage($row["chat_id"], $media["media_content"], BOT_TOKEN);
+        }
+
+        if ( $batch_count == 0 ) {
+                usleep($batch_wait);
+                $batch_count=$batch_volumn;
+        } else {
+                $batch_count--;
+        }
+}
+
+$db = null;
+
+?>
+
---
+++ script/module.php
@@ -0,0 +1,18 @@
+<?php
+
+function sendMessage($chatID, $textMessage, $token) {
+
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = $url . "&text=" . urlencode($textMessage);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+
+?>
+
