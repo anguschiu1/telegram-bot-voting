ed11c2729d6c245273a45f2f3bd0225c91612a03
Goofyz <rwcleung@gmail.com>
2016-05-01 11:33:07 +0800

    Update DB structure

---
+++ database/alter_20160430.sql
@@ -0,0 +1,2 @@
+alter table question add column q4 int, add column q5 int,  add column q6 int,  add column q7 int,  add column q8 varchar(1),  add column q9 int,  add column q10 int,  add column q11 int,  add column q12 varchar(1),  add column q13 int, add column q14 int, add column q15 int;
+alter table voter add column voter2012 int, add column is_voter int, add column age int, add column job int;
